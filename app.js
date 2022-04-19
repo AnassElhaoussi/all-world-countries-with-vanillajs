f1038a5e8d9a838eaaaee17cac5d5632ba12aff5
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-20 01:42:53 +0200

    update : added details page for each country

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const mainSection = document.querySelector(".main-section"),
 labelButton = mainSection.querySelector(".label button"),
 inputField = mainSection.querySelector("input"),
-regionsOptions = mainSection.querySelectorAll(".options a"),
+regionsOptions = mainSection.querySelectorAll(".options a:not(.all)"),
 countriesEl = document.querySelector(".countries-container"),
-toggleBtn = document.querySelector(".modes")
+toggleBtn = document.querySelector(".modes"),
+viewAll = document.querySelector(".all")
+
 let api;
+
 
 
 const displayCountry = countries => {
@@ -13,6 +16,9 @@ const displayCountry = countries => {
     countries.forEach(country => {
     const countryEl = document.createElement('div')
     countryEl.classList.add('card')
+
+    
+
     
     
 
@@ -35,13 +41,52 @@ const displayCountry = countries => {
                         </div>
                     </div>
                 </div>
-   
 `
+countryEl.addEventListener('click', () => {
+    const detailsPage = document.querySelector(".details-container")
+     countriesEl.classList.add('active')
+     mainSection.classList.add("active")
+     detailsPage.classList.remove("active")
 
+    
+    displayDetailsPageCountry(country)
+    
+
+    })
+    
 
     countriesEl.appendChild(countryEl)       
     });
 
+    
+
+}
+
+
+const displayDetailsPageCountry = country => {
+    const detailsBody = document.querySelector(".details-text")
+    const detailsImage = document.querySelector(".details-container img")
+
+    detailsImage.src = country.flag
+
+    detailsBody.innerHTML = `
+    <h3 class="details-country-name">${country.name}</h3>
+                    <div class="population detail">
+                        <span>Population:</span>
+                        <p>${country.population}</p>
+                    </div>
+                    <div class="region detail">
+                        <span>Region:</span>
+                        <p>${country.region}</p>
+                    </div>
+                    <div class="capital detail">
+                        <span>Capital:</span>
+                        <p>${country.capital}</p>
+                    </div>
+
+    `
+
+    
 }
 
 
@@ -92,13 +137,21 @@ inputField.addEventListener('input', e => {
     })
 })
 
+
+
 regionsOptions.forEach(option => {
     option.addEventListener('click', () => {
         console.log(option.innerHTML);
 
     const regionName = document.querySelectorAll(".region")
 
+    
     regionName.forEach(region => {
+ 
+        viewAll.addEventListener('click', () => {
+            region.parentElement.parentElement.parentElement.style.display = 'block'
+        })
+
         if(region.innerHTML.includes(option.innerHTML)){
             region.parentElement.parentElement.parentElement.style.display = "block"
 
@@ -107,14 +160,16 @@ regionsOptions.forEach(option => {
             region.parentElement.parentElement.parentElement.style.display = "none"
 
         }
-        
 
-    })
-        
-       
-        
+
+    })  
+
 
 })
 
 })
+
+
+
+
     
